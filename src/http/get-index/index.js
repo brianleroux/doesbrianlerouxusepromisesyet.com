dc1b284cf35c28a26e38f03460a697ab04994aa9
Mike MacCana <mike@certsimple.com>
2019-01-14 16:44:16 +0000

    Upgrade to arc 4
    Indent HTML
    Remove unused dependencies

---
+++ src/http/get-index/index.js
@@ -0,0 +1,20 @@
+var fs = require("fs"),
+  path = require("path"),
+  css = fs.readFileSync(path.join(__dirname, "index.css")).toString();
+
+exports.handler = async function index(request) {
+  let html = `
+  <!doctype html>
+    <html>
+      <head><style>${css}</style></head>
+    <body>
+      <div class=content><h1>No</h1></div>
+    </body>
+  </html>  
+  `;
+  return {
+    status: 200,
+    type: "text/html; charset=utf-8",
+    body: html
+  };
+};
